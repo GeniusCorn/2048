9ab1593692eb7ec1cf5c0fe60cd6ba671cf4b54f
GeniusCorn <hi@nicecorn.com>
2022-08-28 23:47:16 +0800

    refactor: tile component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,8 @@
 <script setup lang="ts">
-type BoardState = {
-  x: number
-  y: number
-}
+import { TileState } from '@/types/Tile'
+import { state } from '@/types/Board'
 
-const state: BoardState = {
-  x: 4,
-  y: 4
-}
-
-type TileNumber = 0 | 2 | 4 | 8 | 16 | 32 | 64 | 128 | 256 | 512 | 1024 | 2048
-
-type TileState = {
-  x: number
-  y: number
-  number: TileNumber
-  revealed: boolean
-}
+import TileItem from '@/components/TileItem.vue'
 
 const board = Array.from({ length: state.y }, (_, y) =>
   Array.from(
@@ -29,50 +15,6 @@ const board = Array.from({ length: state.y }, (_, y) =>
     })
   )
 )
-
-const tileColors = [
-  'bg-white',
-  'bg-orange-400',
-  'bg-blue-400',
-  'bg-red-400',
-  'bg-pink-400',
-  'bg-violet-400',
-  'bg-amber-400',
-  'bg-purple-400',
-  'bg-rose-400',
-  'bg-teal-400',
-  'bg-emerald-400',
-  'bg-sky-400'
-]
-
-function getTileClass(tile: TileState) {
-  switch (tile.number) {
-    case 2:
-      return tileColors[1]
-    case 4:
-      return tileColors[2]
-    case 8:
-      return tileColors[3]
-    case 16:
-      return tileColors[4]
-    case 32:
-      return tileColors[5]
-    case 64:
-      return tileColors[6]
-    case 128:
-      return tileColors[7]
-    case 256:
-      return tileColors[8]
-    case 512:
-      return tileColors[9]
-    case 1024:
-      return tileColors[10]
-    case 2048:
-      return tileColors[11]
-    default:
-      return tileColors[0]
-  }
-}
 
 function getRandomInt(max: number) {
   return Math.floor(Math.random() * max)
@@ -102,21 +44,8 @@ initTile()
     </div>
     <div border="~ 1 rd-4" shadow-xl bg-slate-400>
       <div flex="~" v-for="(row, y) in board" :key="y">
-        <div
-          m-2
-          min-w-20
-          min-h-20
-          shadow-lg
-          border="~ rd-4"
-          flex="~"
-          justify-center
-          items-center
-          :class="getTileClass(tile)"
-          v-for="(tile, x) in row"
-          :key="x"
-        >
-          {{ tile.number }}
-        </div>
+        <TileItem :number="tile.number" v-for="(tile, x) in row" :key="x">
+        </TileItem>
       </div>
     </div>
     <div>powered by GeniusCorn</div>
---
+++ src/components/TileItem.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+const props = defineProps<{
+  number: number
+}>()
+
+const tileColors = [
+  'bg-white',
+  'bg-orange-400',
+  'bg-blue-400',
+  'bg-red-400',
+  'bg-pink-400',
+  'bg-violet-400',
+  'bg-amber-400',
+  'bg-purple-400',
+  'bg-rose-400',
+  'bg-teal-400',
+  'bg-emerald-400',
+  'bg-sky-400'
+]
+
+function setTileNumberClass(tile: number) {
+  switch (tile) {
+    case 2:
+      return tileColors[1]
+    case 4:
+      return tileColors[2]
+    case 8:
+      return tileColors[3]
+    case 16:
+      return tileColors[4]
+    case 32:
+      return tileColors[5]
+    case 64:
+      return tileColors[6]
+    case 128:
+      return tileColors[7]
+    case 256:
+      return tileColors[8]
+    case 512:
+      return tileColors[9]
+    case 1024:
+      return tileColors[10]
+    case 2048:
+      return tileColors[11]
+    default:
+      return tileColors[0]
+  }
+}
+</script>
+
+<template>
+  <div
+    m-2
+    min-w-20
+    min-h-20
+    shadow-lg
+    border="~ rd-4"
+    flex="~"
+    justify-center
+    items-center
+    :class="setTileNumberClass(props.number)"
+  >
+    {{ props.number }}
+  </div>
+</template>
+
+<style scoped></style>
